236386a3fa7df7cee711ac3ff9c6ad9c2ad949c5
zsm <smzhang@fiberhome.com>
2022-08-25 14:36:53 +0800

    修改例子

--- packages/mini-jackz-vue/examples/Component/main.js
+++ packages/mini-jackz-vue/examples/Component/main.js
@@ -1,6 +1,6 @@
 /* main.js */
 import { App } from './App.js'
-const { createApp } = miniVue
+const { createApp } = miniJackzVue
 const rootContainer = document.querySelector('#app')
 const app = createApp(App)
 app.mount(rootContainer)
