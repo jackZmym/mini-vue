3e1dd67fe5021813dc547efbf716f464d2b4f0b2
zsm <smzhang@fiberhome.com>
2022-08-25 09:40:43 +0800

    release: v1.0.7

--- packages/mini-vue/index.ts
+++ packages/mini-vue/index.ts
@@ -1,7 +1,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/mini-jackz-vue.cjs.prod.js')
+  module.exports = require('./dist/mini-vue.cjs.prod.js')
 } else {
-  module.exports = require('./dist/mini-jackz-vue.cjs.js')
+  module.exports = require('./dist/mini-vue.cjs.js')
 }
