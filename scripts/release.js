0fadf9684a7302121bffa85fc306488b19ab2d42
zsm <smzhang@fiberhome.com>
2022-08-26 11:15:50 +0800

    releasetest: v2.0.2

--- scripts/release.js
+++ scripts/release.js
@@ -124,7 +124,7 @@ async function main() {
   if (stdout) {
     step('\nCommitting changes...')
     await runIfNotDry('git', ['add', '-A'])
-    await runIfNotDry('git', ['commit', '-m', `release: v${targetVersion}`])
+    await runIfNotDry('git', ['commit', '-m', `releasetest: v${targetVersion}`])
   } else {
     console.log('No changes to commit.')
   }
